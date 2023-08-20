af5270bb5918595bf7dd93232778afc362dd1bab
LalitKumar <lalitkumar.vssut@gmail.com>
2023-08-21 01:38:39 +0530

    Chat Queries Uploaded

--- scripts2.js
+++ scripts2.js
@@ -29,3 +29,55 @@ function getParameterByName(name, url) {
     }
   });
   
+
+
+//   // Function to update text display from the server
+// function updateTextDisplay(text) {
+//   var textDisplay = document.querySelector(".text-display");
+//   textDisplay.textContent = text;
+// }
+
+// // Check for user information parameter on page load
+// document.addEventListener("DOMContentLoaded", function() {
+//   var userInfo = getParameterByName("user");
+//   if (userInfo) {
+//     updateUserProfile(JSON.parse(userInfo));
+//   }
+
+//   // Example: Simulate text received from the server
+//   var textFromServer = "Hello! This is some text from the server.";
+//   updateTextDisplay(textFromServer);
+// });
+
+
+
+
+// Function to send queries and update UI with response
+function sendQueryAndDisplayResponse(query) {
+  // Send the query to Firestore and listen for changes
+  var queryRef = db.collection("queries").doc();
+  queryRef.set({ query: query });
+
+  // Listen for changes in the response document
+  queryRef.collection("response").doc("responseDoc").onSnapshot(function(snapshot) {
+    var response = snapshot.data().response;
+    updateTextDisplay(response);
+  });
+}
+
+// Listen for query button click
+document.getElementById("query-button").addEventListener("click", function() {
+  var queryInput = document.getElementById("query-input");
+  var query = queryInput.value.trim();
+  if (query !== "") {
+    sendQueryAndDisplayResponse(query);
+  }
+});
+
+// Initialize page and user profile
+document.addEventListener("DOMContentLoaded", function() {
+  var userInfo = getParameterByName("user");
+  if (userInfo) {
+    updateUserProfile(JSON.parse(userInfo));
+  }
+});
